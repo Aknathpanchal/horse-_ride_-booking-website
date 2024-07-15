9c0a8135ebad03f4b6962bac4d228db7a91c36b7
Shreenath Panchal <shree.d.panchal1999@gmail.com>
2024-07-15 17:22:21 +0530

    Data added

---
+++ src/Data/horseData.jsx
@@ -0,0 +1,6 @@
+export const horses = [
+    { name: "Chetak ", image: "https://cdn.pixabay.com/photo/2017/12/10/15/16/white-horse-3010129_1280.jpg" },
+    { name: "Badal ", image: "https://cdn.pixabay.com/photo/2016/12/16/13/20/horse-1911400_1280.jpg" },
+    { name: "Pavan ", image: "https://cdn.pixabay.com/photo/2022/05/21/15/17/horse-7211696_1280.jpg" },
+    { name: "Sultan ", image: "https://cdn.pixabay.com/photo/2018/05/11/11/11/horse-3390256_1280.jpg" },
+  ];
